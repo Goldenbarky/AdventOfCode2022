4b365ba99424039524bd5b7150dc560b39d93c24
Justin Kennedy <kennedjustin7@gmail.com>
2022-11-30 23:35:00 -0500

    Initial commit

---
+++ Days/Day1.cs
@@ -0,0 +1,17 @@
+class Day1 {
+
+    public static void Part1(StreamReader sr) {
+        string line = "";
+        while(line != null){
+            line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+    }
+
+    public static void Part2(StreamReader sr) {
+        string line = "";
+        while(line != null){
+            line = sr.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿class Program {
+
+    static string FILE_NAME = "Input/input{0}.txt";
+    static string TEST_FILE_NAME = "Input/input{0}sample.txt";
+
+    public static void Main(string[] args) {
+
+        string day = args[0];
+        string part = args[1];
+
+        Type type = Type.GetType(string.Concat("Day", day));
+        var method = type.GetMethod(string.Concat("Part", part));
+        string fileName = string.Format(
+            args.Length >= 3 && args[2] == "test"
+                ? TEST_FILE_NAME
+                : FILE_NAME,
+            day
+        );
+        
+        try {
+            StreamReader sr = new StreamReader(fileName);
+            object[] arguments = {sr};
+
+            method.Invoke(null, arguments);
+        } catch(FileNotFoundException) {
+            Console.WriteLine($"Oops! File {fileName} does not exist yet");
+            return;
+        }
+    }
+
+    public static int[] StringArrayToInt(string[] input) {
+        int[] array = new int[input.Count()];
+        for(int i = 0; i < input.Count(); i++) {
+            array[i] = int.Parse(input[i]);
+        }
+
+        return array;
+    }
+}
