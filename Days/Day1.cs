4b4f8f8ffb838718f1a8055704ad2cfc392a92b7
Justin Kennedy <kennedjustin7@gmail.com>
2022-12-01 00:49:21 -0500

    Day 1 completed & template upgraded

--- Days/Day1.cs
+++ Days/Day1.cs
@@ -1,17 +1,52 @@
 class Day1 {
 
     public static void Part1(StreamReader sr) {
-        string line = "";
+        string line = sr.ReadLine();
+        List<int> elves = new List<int>();
+        elves.Add(0);
+        int elfIndex = 0;
         while(line != null){
+            if(line != "") {
+                elves[elfIndex] += int.Parse(line);
+            } else {
+                elfIndex++;
+                elves.Add(0);
+            }
             line = sr.ReadLine();
-            Console.WriteLine(line);
         }
+
+        PriorityQueue<int, int> bigElves = new System.Collections.Generic.PriorityQueue<int, int>();
+        int max = int.MinValue;
+        foreach(int elf in elves) {
+            if(elf > max) max = elf;
+        }
+
+        Console.WriteLine(max);
     }
 
     public static void Part2(StreamReader sr) {
-        string line = "";
+        string line = sr.ReadLine();
+        List<int> elves = new List<int>();
+        elves.Add(0);
+        int elfIndex = 0;
         while(line != null){
+            if(line != "") {
+                elves[elfIndex] += int.Parse(line);
+            } else {
+                elfIndex++;
+                elves.Add(0);
+            }
             line = sr.ReadLine();
         }
+
+        elves.Sort();
+        elves.Reverse();
+
+        int total = 0;
+        for( int i = 0; i < 3; i++) {
+            total += elves[i];
+        }
+
+        Console.WriteLine(total);
     }
 }
